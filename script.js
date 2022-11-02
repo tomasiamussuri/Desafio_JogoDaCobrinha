6c9bf48e366ef39d91cd60180f826c28fe218391
tomasia.mussuri <tomasia.mussuri@gmail.com>
2022-11-02 11:35:40 -0300

    update new functions

--- script.js
+++ script.js
@@ -7,10 +7,14 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box,
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 10 * box, 10 * box);
+    context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha() {
@@ -20,16 +24,19 @@ function criarCobrinha() {
     }
 }
 
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
 document.addEventListener('keydown', update);
 
 function update (event) {
-    if(event.keyCode == 37 && direction != "right") direction = "left";
-    if(event.keyCode == 38 && direction != "down") direction = "up";
-    if(event.keyCode == 39 && direction != "left") direction = "right";
-    if(event.keyCode == 40 && direction != "up") direction = "down";
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'up') direction = 'down';
 }
-
-
 
 function iniciarJogo() {
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
@@ -39,14 +46,15 @@ function iniciarJogo() {
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
     if(direction == "rigth") snakeX +=box;
-    if(direction == "left") snakeX -=box;
+    if(direction == 'left') snakeX -=box;
     if(direction == "up") snakeY -=box;
-    if(direction == "down") snakeY +=box;
+    if(direction == 'down') snakeY +=box;
 
     snake.pop(1);
 
