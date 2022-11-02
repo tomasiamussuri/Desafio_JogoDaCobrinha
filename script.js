11d87975f94d66d12cb97d6f3a580d75b6a6f704
tomasia.mussuri <tomasia.mussuri@gmail.com>
2022-11-01 23:14:13 -0300

    update new functions

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
-
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+let direction = "right";
 
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 10 * box, 10 * box);
 }
 
-criarBG();
+
+function criarCobrinha() {
+    for(i = 0; i < snake.length; i++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+function iniciarJogo() {
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "rigth") snakeX +=box;
+    if(direction == "left") snakeX -=box;
+    if(direction == "up") snakeY -=box;
+    if(direction == "down") snakeY +=box;
+
+    snake.pop(1);
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
